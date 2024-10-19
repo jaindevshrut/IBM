9ad533912d2695753ac782f44e4ce53df20e6411
Nilangbhuva <nilangbhuva694@gmail.com>
2024-10-20 02:00:19 +0530

    ISL to text

---
+++ ISL to Text/Models/new_model.py
@@ -0,0 +1,35 @@
+from Data.load_data import get_train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dropout, Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping
+
+train_data, test_data, train_target, test_target = get_train_test_split()
+
+model = Sequential()
+
+model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=(100, 100, 1)))
+model.add(MaxPool2D(pool_size=(2, 2), strides=2))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'))
+model.add(MaxPool2D(pool_size=(2, 2), strides=2))
+model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='valid'))
+model.add(MaxPool2D(pool_size=(2, 2), strides=2))
+
+model.add(Flatten())
+model.add(Dropout(0.5))
+
+model.add(Dense(64, activation="relu"))
+model.add(Dense(128, activation="relu"))
+model.add(Dense(128, activation="relu"))
+model.add(Dense(36, activation="softmax"))
+
+model.compile(optimizer=Adam(learning_rate=0.00001), loss='categorical_crossentropy', metrics=['accuracy'])
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=1, min_lr=0.0005)
+early_stop = EarlyStopping(monitor='val_loss', min_delta=0, patience=2, verbose=0, mode='auto')
+
+history = model.fit(train_data, train_target, epochs=15, callbacks=[reduce_lr, early_stop], validation_split=0.3)
+model.save("C:/Users/dubey/OneDrive/Desktop/Coding/Projects/ISL-to-Text-Converter/models/ISL_model.keras")
+print(model.summary())
+
+model.test_on_batch(test_data, test_target)
+print(model.metrics_names)
---
+++ ISL to Text/live camera detection/gesture_detection.py
@@ -0,0 +1,121 @@
+import numpy as np
+import cv2
+from tensorflow import keras as tf
+
+# Ensure that the path is correct and the .keras file is compatible
+try:
+    # Use raw strings to avoid issues with backslashes
+    model = tf.models.load_model(r"C:\Users\dubey\OneDrive\Desktop\Coding\Projects\ISL-to-Text-Converter\Models\ISL_model.keras")
+    print("Model loaded successfully")
+except Exception as e:
+    print(f"Error loading model: {e}")
+     
+background = None
+accumulated_weight = 0.5
+ROI_top = 10
+ROI_bottom = 300
+ROI_right = 400
+ROI_left = 630
+
+label_dict = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9',
+              10: 'a', 11: 'b', 12: 'c', 13: 'd', 14: 'e', 15: 'f', 16: 'g', 17: 'h', 18: 'i', 19: 'j', 20: 'k', 21: 'l', 22: 'm',
+              23: 'n', 24: 'o', 25: 'p', 26: 'q', 27: 'r', 28: 's', 29: 't', 30: 'u', 31: 'v', 32: 'w', 33: 'x', 34: 'y', 35: 'z'}
+
+def cal_accum_avg(frame, accumulated_weight):
+    global background
+
+    if background is None:
+        background = frame.copy().astype("float")
+        return None
+
+    cv2.accumulateWeighted(frame, background, accumulated_weight)
+
+
+def segment_hand(frame, threshold=25):
+    global background
+
+    diff = cv2.absdiff(background.astype("uint8"), frame)
+
+    _, thresholded = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)
+
+    # Fetching contours in the frame (These contours can be of hand or any other object in foreground) ...
+    contours, image = cv2.findContours(thresholded.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # If length of contours list = 0, means we didn't get any contours...
+    if len(contours) == 0:
+        return None
+    else:
+        # The largest external contour should be the hand
+        hand_segment_max_cont = max(contours, key=cv2.contourArea)
+
+        # Returning the hand segment(max contour) and the thresholded image of hand...
+        return (thresholded, hand_segment_max_cont)
+
+
+cam = cv2.VideoCapture(0)
+num_frames = 0
+while True:
+    ret, frame = cam.read()
+
+    # flipping the frame to prevent inverted image of captured frame...
+    frame = cv2.flip(frame, 1)
+
+    frame_copy = frame.copy()
+
+    # ROI from the frame
+    roi = frame[ROI_top:ROI_bottom, ROI_right:ROI_left]
+
+    gray_frame = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    gray_frame = cv2.GaussianBlur(gray_frame, (9, 9), 0)
+
+    if num_frames < 70:
+
+        cal_accum_avg(gray_frame, accumulated_weight)
+
+        cv2.putText(frame_copy, "PLEASE WAIT", (80, 400), cv2.FONT_HERSHEY_SIMPLEX, 0.9,
+                    (0, 255, 0), 2)
+
+    else:
+        # segmenting the hand region
+        hand = segment_hand(gray_frame)
+
+        # Checking if we are able to detect the hand...
+        if hand is not None:
+            thresholded, hand_segment = hand
+
+            # Drawing contours around hand segment
+            cv2.drawContours(frame_copy, [hand_segment + (ROI_right, ROI_top)], -1, (255, 0, 0), 1)
+
+            cv2.imshow("Threshold Hand Image", thresholded)
+
+            thresholded = cv2.resize(thresholded, (100, 100))
+            # thresholded = cv2.cvtColor(thresholded, cv2.COLOR_GRAY2RGB)
+            thresholded = thresholded.reshape(1, 100, 100, 1)
+
+            pred = model.predict(thresholded)
+            cv2.putText(frame_copy, label_dict[np.argmax(pred)], (170, 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+    # Draw ROI on frame_copy
+    cv2.rectangle(frame_copy, (ROI_left, ROI_top), (ROI_right, ROI_bottom), (255, 128, 0), 3)
+
+    # incrementing the number of frames for tracking
+    num_frames += 1
+
+    # Display the frame with segmented hand
+    cv2.putText(frame_copy, "Live feed hand gesture recognition", (10, 20), cv2.FONT_ITALIC, 0.6, (51, 255, 51), 1)
+    cv2.imshow("Sign Detection", frame_copy)
+
+    # Close windows with Esc
+    k = cv2.waitKey(1) & 0xFF
+
+    if k == 27:
+        break
+    
+converter = tf.lite.TFLiteConverter.from_saved_model('Projects\ISL-to-Text-Converter\Models\ISL_model.keras')
+tflite_model = converter.convert()
+with open('model.tflite', 'wb') as f:
+    f.write(tflite_model)
+
+# Release the camera and destroy all the windows
+cam.release()
+cv2.destroyAllWindows()
